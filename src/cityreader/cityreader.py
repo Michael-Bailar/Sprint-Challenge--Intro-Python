19d3c4aaab9af4eeff9d809b1b485e300e73ea0e
Michael-B1764 <mikebailar@gmail.com>
2020-07-10 10:19:36 -0700

    finished at MVP no stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -16,12 +16,35 @@
 # should not be loaded into a City object.
 cities = []
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+
+  with open('cities.csv') as csv_file:
+    import csv
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+      if line_count == 0:
+        line_count += 1
+      else:
+        city = City(row[0], float(row[3]), float(row[4]))
+        cities.append(city)
+        line_count += 1
+      
     return cities
+
+
 
 cityreader(cities)
 
